5d777f1f7d81762a6bf9fd146f007f81fd9c8398
elshaddd <elshadisaev48@gmail.com>
2024-11-26 17:03:18 +0300

    scripts added

---
+++ bin_to_tif.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+INPUT_FILE="$1"
+OUTPUT_FILE="$2"
+
+# Название исходного файла с кодом и исполняемого файла
+SOURCE_FILE="src/bin_to_tif.cpp"
+EXECUTABLE_FILE="app/bin_to_tif"
+
+# Проверяем наличие исходного кода
+if [[ ! -f "$SOURCE_FILE" ]]; then
+    echo "Ошибка: Файл с исходным кодом $SOURCE_FILE не найден!"
+    exit 1
+fi
+
+# Компилируем программу
+echo "Компиляция программы..."
+g++ -o "$EXECUTABLE_FILE" "$SOURCE_FILE" -ltiff
+if [[ $? -ne 0 ]]; then
+    echo "Ошибка: Компиляция не удалась!"
+    exit 1
+fi
+echo "Компиляция успешна."
+
+# Проверяем существование входного файла
+if [[ ! -f "$INPUT_FILE" ]]; then
+    echo "Ошибка: Входной файл $INPUT_FILE не существует!"
+    exit 1
+fi
+
+# Запускаем программу
+echo "Запуск программы..."
+./"$EXECUTABLE_FILE" "$INPUT_FILE" "$OUTPUT_FILE"
